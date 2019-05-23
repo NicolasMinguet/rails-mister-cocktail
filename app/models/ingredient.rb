b9d1af2f3c1fc31fd73b6653e8d3dbac67a1f9c1
Nicolas Minguet <nicolas.minguet@hotmail.fr>
2019-05-23 12:41:46 +0200

    made it through specs

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,14 @@
 class Ingredient < ApplicationRecord
+  before_destroy :ingredient_used?
+  has_many :doses
+  validates :name, uniqueness: true, presence: true
 
+
+  private
+
+  def ingredient_used?
+    errors.add(:base, "Cannot delete booking with payments") unless Cocktail.count == 0
+
+    errors.blank? #return false, to not destroy the element, otherwise, it will delete.
+  end
 end
